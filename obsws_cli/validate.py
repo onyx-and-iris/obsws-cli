e4ab4ae630e7b21003c2a6c5e092995d9a82de58
onyx-and-iris <code@onyxandiris.online>
2026-01-09 23:22:14 +0000

    remove unused monitor_exists() function

--- obsws_cli/validate.py
+++ obsws_cli/validate.py
@@ -97,12 +97,6 @@ def profile_not_exists(ctx: typer.Context, profile_name: str) -> str:
     return profile_name
 
 
-def monitor_exists(ctx: typer.Context, monitor_index: int) -> bool:
-    """Check if a monitor exists."""
-    resp = ctx.obj['obsws'].get_monitor_list()
-    return any(monitor['monitorIndex'] == monitor_index for monitor in resp.monitors)
-
-
 def kind_in_input_kinds(ctx: typer.Context, input_kind: str) -> str:
     """Check if an input kind is valid."""
     resp = ctx.obj['obsws'].get_input_kind_list(False)
