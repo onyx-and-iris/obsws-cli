5355d29a31a1e8b991c6107b17c2439724ec7636
onyx-and-iris <code@onyxandiris.online>
2026-01-10 14:04:27 +0000

    keep it consistent

--- obsws_cli/sceneitem.py
+++ obsws_cli/sceneitem.py
@@ -30,7 +30,7 @@ def list_(
     uuid: Annotated[bool, typer.Option(help='Show UUIDs of scene items')] = False,
 ):
     """List all items in a scene."""
-    if not scene_name:
+    if scene_name is None:
         scene_name = ctx.obj['obsws'].get_current_program_scene().scene_name
 
     resp = ctx.obj['obsws'].get_scene_item_list(scene_name)
